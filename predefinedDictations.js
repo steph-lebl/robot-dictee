c484b42381f967277f882f59f212851c45bcbf23
Stéphane Leblanc <stephane.leblanc@ville.montreal.qc.ca>
2021-01-06 14:37:04 -0500

    More predefined dictations

--- predefinedDictations.js
+++ predefinedDictations.js
@@ -20,6 +20,47 @@
         title:'Son "ui"',
         text:'La pluie tombera toute la nuit. Une maman prépare une salade de fruits et fait cuire les biscuits.'
       },
+      {
+        title:'Son "o"',
+        text:'Aujourd\'hui, Caroline porte un manteau jaune. J\'écris les mots soleil et animal au tableau.'
+      },
+      {
+        title:'Son "é"',
+        text:'Après le diner, vous pourrez jouer à la poupée. Chaque année les pompiers viennent à l\'école. J\'ai passé une journée de congé agréable chez Émile.'
+      },
+      {
+        title:'Son "è"',
+        text:'Pour la fête de Noël les jumelles souhaitent avoir beaucoup de jouets. Ma mère n\'aime pas l\'hiver avec ses tempêtes de neige. Il est un excellent cuisinier.'
+      },
+      {
+        title:'Son "s"',
+        text:'Tous les samedis, ce monsieur prend sa bicyclette pour aller au cinéma. Ce garçon verse ses céréales dans une grosse tasse.'
+      },
+      {
+        title:'Son "z"',
+        text:'Je mange des raisins et des fraises dans la cuisine. Ma cousine porte une blouse rose et une jupe grise. Il fait zéro degré.'
+      },
+
+      {
+        title:'Son "j"',
+        text:'La gentille magicienne jongle avec des oranges. Les jumeaux jouent dans le jardin tous les jours.'
+      },
+      {
+        title:'Son "ch"',
+        text:'Il cherche son chandail et son chapeau. Dans le champ, je vois une vache, un cheval, une chèvre et un cochon.'
+      },
+      {
+        title:'Son "on"',
+        text:'Le garçon joue avec son camion de pompier dans le salon. Des flocons de neige tombent sur la montagne.'
+      },
+      {
+        title:'Son "ou"',
+        text:'Ma cousine joue avec ses poupées ou ses toutous. Où sont mes souliers de couleur rouge?'
+      },
+      {
+        title:'Son "eu"',
+        text:'Ma soeur veut un oeuf au déjeuner. Cette jeune chanteuse est heureuse, car elle a reçu des fleurs bleues.'
+      },
     ]
   
   })(this)  
