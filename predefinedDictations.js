d3ef147c9ada09d58decfa696f62ce1415c9e84c
Stéphane Leblanc <stephane.leblanc@montreal.ca>
2025-11-16 13:36:50 -0500

    feat: first word list from maxime

--- predefinedDictations.js
+++ predefinedDictations.js
@@ -1,83 +1,90 @@
-(function(global){
-    global.robotDictee.predefinedDictations=[
-      {
-        title:'Son "a"',
-        text:'Mon ami joue à la balle avec son chat. Sur la table, il y a un gâteau au chocolat et des cadeaux.'
-      },
-      {
-        title:'Son "e"',
-        text:'Mon petit frère s\'amuse avec son amie Rose. Il y a une jolie tortue sur mon chemin.'
-      },
-      {
-        title:'Son "i"',
-        text:'Je fais de la bicyclette tous les lundis. Les images de ce livre me font rire.'
-      },
-      {
-        title:'Son "u"',
-        text:'Bruno boit un jus de légumes. Julie aime la musique. Elle joue de la flue.'
-      },
-      {
-        title:'Son "ui"',
-        text:'La pluie tombera toute la nuit. Une maman prépare une salade de fruits et fait cuire les biscuits.'
-      },
-      {
-        title:'Son "o"',
-        text:'Aujourd\'hui, Caroline porte un manteau jaune. J\'écris les mots soleil et animal au tableau.'
-      },
-      {
-        title:'Son "é"',
-        text:'Après le diner, vous pourrez jouer à la poupée. Chaque année les pompiers viennent à l\'école. J\'ai passé une journée de congé agréable chez Émile.'
-      },
-      {
-        title:'Son "è"',
-        text:'Pour la fête de Noël les jumelles souhaitent avoir beaucoup de jouets. Ma mère n\'aime pas l\'hiver avec ses tempêtes de neige. Il est un excellent cuisinier.'
-      },
-      {
-        title:'Son "s"',
-        text:'Tous les samedis, ce monsieur prend sa bicyclette pour aller au cinéma. Ce garçon verse ses céréales dans une grosse tasse.'
-      },
-      {
-        title:'Son "z"',
-        text:'Je mange des raisins et des fraises dans la cuisine. Ma cousine porte une blouse rose et une jupe grise. Il fait zéro degré.'
-      },
-      {
-        title:'Son "j"',
-        text:'La gentille magicienne jongle avec des oranges. Les jumeaux jouent dans le jardin tous les jours.'
-      },
-      {
-        title:'Son "ch"',
-        text:'Il cherche son chandail et son chapeau. Dans le champ, je vois une vache, un cheval, une chèvre et un cochon.'
-      },
-      {
-        title:'Son "on"',
-        text:'Le garçon joue avec son camion de pompier dans le salon. Des flocons de neige tombent sur la montagne.'
-      },
-      {
-        title:'Son "ou"',
-        text:'Ma cousine joue avec ses poupées ou ses toutous. Où sont mes souliers de couleur rouge?'
-      },
-      {
-        title:'Son "eu"',
-        text:'Ma soeur veut un oeuf au déjeuner. Cette jeune chanteuse est heureuse, car elle a reçu des fleurs bleues.'
-      },
+(function (global) {
+  global.robotDictee.predefinedDictations = [
+    {
+      title: 'Thème 2 - semaine 5',
+      text: `âge changer danger genou genre jeter jeune ménage nuage passage village bonjour janvier joue jus rejoindre`
+    },
+    /*      
+    
+    
+          {
+            title:'Son "a"',
+            text:'Mon ami joue à la balle avec son chat. Sur la table, il y a un gâteau au chocolat et des cadeaux.'
+          },
+          {
+            title:'Son "e"',
+            text:'Mon petit frère s\'amuse avec son amie Rose. Il y a une jolie tortue sur mon chemin.'
+          },
+          {
+            title:'Son "i"',
+            text:'Je fais de la bicyclette tous les lundis. Les images de ce livre me font rire.'
+          },
+          {
+            title:'Son "u"',
+            text:'Bruno boit un jus de légumes. Julie aime la musique. Elle joue de la flue.'
+          },
+          {
+            title:'Son "ui"',
+            text:'La pluie tombera toute la nuit. Une maman prépare une salade de fruits et fait cuire les biscuits.'
+          },
+          {
+            title:'Son "o"',
+            text:'Aujourd\'hui, Caroline porte un manteau jaune. J\'écris les mots soleil et animal au tableau.'
+          },
+          {
+            title:'Son "é"',
+            text:'Après le diner, vous pourrez jouer à la poupée. Chaque année les pompiers viennent à l\'école. J\'ai passé une journée de congé agréable chez Émile.'
+          },
+          {
+            title:'Son "è"',
+            text:'Pour la fête de Noël les jumelles souhaitent avoir beaucoup de jouets. Ma mère n\'aime pas l\'hiver avec ses tempêtes de neige. Il est un excellent cuisinier.'
+          },
+          {
+            title:'Son "s"',
+            text:'Tous les samedis, ce monsieur prend sa bicyclette pour aller au cinéma. Ce garçon verse ses céréales dans une grosse tasse.'
+          },
+          {
+            title:'Son "z"',
+            text:'Je mange des raisins et des fraises dans la cuisine. Ma cousine porte une blouse rose et une jupe grise. Il fait zéro degré.'
+          },
+          {
+            title:'Son "j"',
+            text:'La gentille magicienne jongle avec des oranges. Les jumeaux jouent dans le jardin tous les jours.'
+          },
+          {
+            title:'Son "ch"',
+            text:'Il cherche son chandail et son chapeau. Dans le champ, je vois une vache, un cheval, une chèvre et un cochon.'
+          },
+          {
+            title:'Son "on"',
+            text:'Le garçon joue avec son camion de pompier dans le salon. Des flocons de neige tombent sur la montagne.'
+          },
+          {
+            title:'Son "ou"',
+            text:'Ma cousine joue avec ses poupées ou ses toutous. Où sont mes souliers de couleur rouge?'
+          },
+          {
+            title:'Son "eu"',
+            text:'Ma soeur veut un oeuf au déjeuner. Cette jeune chanteuse est heureuse, car elle a reçu des fleurs bleues.'
+          },
+    
+          {
+            title:'Sons "un" et "oi"',
+            text:'J\'ai vu trois étoiles filantes lundi soir. Il y a aucun oiseau brun dans ce bois.'
+          },
+          {
+            title:'Son "oin"',
+            text:'Je prends soin des animaux. Mon oncle qui habite au loin téléphone au moins une fois par semaine.'
+          },
+          {
+            title:'Son "in"',
+            text:'Mon cousin portera son imperméable demain. Mon chien n\'a plus faim. Il a le ventre plein.'
+          },
+          {
+            title:'Son "ill"',
+            text:'Le soleil brille à travers le vitrail; une merveille! La fille boit son jus avec une paille. Sur ma feuille, je dessine un écureuil, une grenouille et un papillon. Je cherche mes crayons. Je suis joyeux. Je pars en voyage découvrir un nouveau pays.'
+          }
+    */
+  ]
 
-      {
-        title:'Sons "un" et "oi"',
-        text:'J\'ai vu trois étoiles filantes lundi soir. Il y a aucun oiseau brun dans ce bois.'
-      },
-      {
-        title:'Son "oin"',
-        text:'Je prends soin des animaux. Mon oncle qui habite au loin téléphone au moins une fois par semaine.'
-      },
-      {
-        title:'Son "in"',
-        text:'Mon cousin portera son imperméable demain. Mon chien n\'a plus faim. Il a le ventre plein.'
-      },
-      {
-        title:'Son "ill"',
-        text:'Le soleil brille à travers le vitrail; une merveille! La fille boit son jus avec une paille. Sur ma feuille, je dessine un écureuil, une grenouille et un papillon. Je cherche mes crayons. Je suis joyeux. Je pars en voyage découvrir un nouveau pays.'
-      }
-    ]
-  
-  })(this)  
-  
+})(this)
