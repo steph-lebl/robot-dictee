3fbb06f9d81aaaba1bfa030432adfd82c708c523
Stéphane Leblanc <stephane.leblanc@montreal.ca>
2025-11-25 19:58:42 -0500

    feat: all theme

--- predefinedDictations.js
+++ predefinedDictations.js
@@ -1,26 +1,105 @@
 (function (global) {
   global.robotDictee.predefinedDictations = [
     {
-      title: `Semaine 1`,
-      text: `bord\nbut\nchant\ndoigt\ndroit\ndroite\nfond\nhaut\nméchant\nméchante\nmort\nmorte\nnid\npot\nrat\nrate\nrenard\nrenarde\nretard\nrond\nronde\nsalut`
+      "title": "Thème 1 - Semaine 1",
+      "text": "salade\nsec\nsèche\nsecouer\nsentier\nsentir\nservice\nsoie\nsoin\nson\nsoulier\nsouper\nsouriant\nsouriante\nspectacle\nsucre\nsucrerie\nsurface"
     },
     {
-      title: `Semaine 2`,
-      text: `ajout\nalphabet\nargent\ncourt\ncourte\ndos\népais\népaisse\ngentil\ngentille\nimportant\nimportante\nmauvais\nmauvaise\nminuit\nouvert\nouverte\npart\npoint\nprécis\nprécise\nsabot\nsourcil`
+      "title": "Thème 1 - Semaine 2",
+      "text": "aréna\ncaméra\ncinéma\ndéposer\nécouter\nespérer\nétoile\nfévrier\nmétéo\nmétier\nposséder\nprécipiter\npréparer\nprésenter\nprotéger\ntrésor"
     },
     {
-      title: `Semaine 3`,
-      text: `ampoule\nchamp\ncombien\ncomprendre\ncompter\ndécembre\nembrasser\nensemble\nexemple\njambe\nlongtemps\nnombre\nnovembre\nombre\nremplir\nsembler\nseptembre`
+      "title": "Thème 1 - Semaine 3",
+      "text": "cacher\ncamion\ncoin\ncommencer\nconsoler\ncontinuer\ncontre\ncostume\ncoucher\ncourant\ncours\ncourse\navancer\nglace\nlancer\nnaissance"
     },
     {
-      title: `Semaine 4`,
-      text: `bande\nbarbe\ncuisine\ndanse\ndemande\ndemie\nlibre\nmaladie\nmine\nmurmure\npâle\npartie\nperdre\npile\npoudre\ntimide\nvivre\nvoiture\nvue`
+      "title": "Thème 1 - Semaine 4",
+      "text": "abri\namour\navion\navril\nbâton\nbonsoir\ncar\ncri\ndemi\ndemie\ndur\ndure\nlion\nlionne\nmal\nmari\nmars\nmur\nor\nours\nourse\npoil\ntour\ntrou"
     },
     {
-      title: `Semaine 5`,
-      text: `âge\nchanger\ndanger\ngenou\ngenre\njeter\njeune\nménage\nnuage\npassage\nvillage\nbonjour\njanvier\njoue\njus\nrejoindre`
+      "title": `Thème 2 - Semaine 1`,
+      "text": `bord\nbut\nchant\ndoigt\ndroit\ndroite\nfond\nhaut\nméchant\nméchante\nmort\nmorte\nnid\npot\nrat\nrate\nrenard\nrenarde\nretard\nrond\nronde\nsalut`
+    },
+    {
+      "title": `Thème 2 - Semaine 2`,
+      "text": `ajout\nalphabet\nargent\ncourt\ncourte\ndos\népais\népaisse\ngentil\ngentille\nimportant\nimportante\nmauvais\nmauvaise\nminuit\nouvert\nouverte\npart\npoint\nprécis\nprécise\nsabot\nsourcil`
+    },
+    {
+      "title": `Thème 2 - Semaine 3`,
+      "text": `ampoule\nchamp\ncombien\ncomprendre\ncompter\ndécembre\nembrasser\nensemble\nexemple\njambe\nlongtemps\nnombre\nnovembre\nombre\nremplir\nsembler\nseptembre`
+    },
+    {
+      "title": `Thème 2 - Semaine 4`,
+      "text": `bande\nbarbe\ncuisine\ndanse\ndemande\ndemie\nlibre\nmaladie\nmine\nmurmure\npâle\npartie\nperdre\npile\npoudre\ntimide\nvivre\nvoiture\nvue`
+    },
+    {
+      "title": `Thème 2 - Semaine 5`,
+      "text": `âge\nchanger\ndanger\ngenou\ngenre\njeter\njeune\nménage\nnuage\npassage\nvillage\nbonjour\njanvier\njoue\njus\nrejoindre`
+    },
+    {
+      "title": "Thème 3 - Semaine 1",
+      "text": "chaise\nchoisir\ncrise\néglise\nfraise\nposer\ntélévision\nusine\nchasse\nchasser\nchasseur\nchasseuse\nglisser\npossible\npousser\npoussin\nprofesseur\nprofesseure\nréussir\nvitesse"
+    },
+    {
+      "title": "Thème 3 - Semaine 2",
+      "text": "baigner\ngagner\nligne\nsoigner\ndiagonal\ndiagonale\ngarder\ngauche\nglacier\ngorge\ngoutte\ngroupe\nmagasin\ndéguiser\nguerre\nguidon\nlangue"
+    },
+    {
+      "title": "Thème 3 - Semaine 3",
+      "text": "aider\navaler\ncrier\ndiriger\nmener\nmontrer\noublier\nporter\nraconter\nremonter\nretourner\nretrouver\nrouler\nterminer\ntirer\ntraverser\ntuer\nverser"
+    },
+    {
+      "title": "Thème 3 - Semaine 4",
+      "text": "année\naraignée\nbouchée\npurée\nrandonnée\nrangée\nsoirée\ntraversée\nveillée\nclé\nmoitié\nsanté\nassez\ncongé\nmai\nmarché"
+    },
+    {
+      "title": "Thème 4 - Semaine 1",
+      "text": "craquelin\ndès\nplastique\npresque\nquel\nquelle\nquels\nquelles\nquelqu'un\nquelqu'une\nquelques-uns\nquelques-unes\nremarquer\nsans\ntaquiner\ntranquille\ntranquillité\nparc\nbien\nbientôt\nrien\nterrien\nterrienne\nvégétarien\nvégétarienne"
+    },
+    {
+      "title": "Thème 4 - Semaine 2",
+      "text": "bonheur\nbricoleur\nbricoleuse\nchanteur\nchanteuse\ncouleur\ndocteur\ndocteure\nmalheur\nmeilleur\nmeilleure\namoureux\namoureuse\nchaleureux\nchaleureuse\ndouloureux\ndouloureuse\nlieu\nmilieu\nbrutal\nbrutale\nhôpital\njournal\nmétal\nmétaux"
+    },
+    {
+      "title": "Thème 4 - Semaine 3",
+      "text": "bonhomme\nhabiter\nhauteur\nherbe\nhorrible\nacheter\nbouchon\nbranche\nchacun\nchacune\nmachine\nmarcher\npenche\npoche\nrecherche\nriche\ntoucher"
+    },
+    {
+      "title": "Thème 4 - Semaine 4",
+      "text": "adjectif\nfacile\nfarine\nferme\nfermer\nfil\nfilm\nfixe\nfleuve\nfou\nfolle\nneuf\nneuve\npréférer\ndauphin\nphoto\ntéléphone\ntéléphoner"
+    },
+    {
+      "title": "Thème 5 - Semaine 5",
+      "text": "aussitôt\nautobus\ncause\nfaute\noctobre\npromener\nrestaurant\nsauver\nagneau\nbateau\nbureau\ncadeau\nchâteau\nmorceau\nmuseau\nruisseau\nradio"
+    },
+    {
+      "title": "Thème 5 - Semaine 1",
+      "text": "après-midi\nclair\nclaire\ncrème\nfrais\nfraîche\nlaisser\nlèvre\nlièvre\nlumière\nmètre\nparaître\npièce\nrivière\nsaison\nsouhaiter\ntroisième\nvinaigre"
+    },
+    {
+      "title": "Thème 5 - Semaine 2",
+      "text": "bête\enquête\nrêver\nvêtement\nbracelet\ncornet\njouet\njuillet\nsujet\nfer\nfier\nfière\nmer\naoût\nd'abord\nfaim\njuin\nzoo"
+    },
+    {
+      "title": "Thème 5 - Semaine 3",
+      "text": "affaire\napporter\napprocher\nattraper\nbattre\nbotte\nchiffre\nfrapper\nnourriture\noccuper\npermettre\npierre\npromettre\nrappeler\nremettre\ntrottoir\nverre"
+    },
+    {
+      "title": "Thème 5 - Semaine 4",
+      "text": "bouteille\nbrouillon\ncheville\nréveiller\ntaille\ntravailler\nmoyen\npayer\nrayon\nvoyage\nallure\nballe\nballon\nmille\nmillier\nmillion\nsalle"
+    },
+    {
+      "title": "Thème 6 - Semaine 1",
+      "text": "bain\ncertain\ncertaine\nchagrin\ndemain\nlendemain\npatin\npin\nprochain\nprochaine\nsapin\nterrain\nvoisin\nvoisine\nappartement\nlentement\nattention\ninvitation\nquestion\nréaction"
+    },
+    {
+      "title": "Thème 6 - Semaine 2",
+      "text": "amande\napprendre\navant-midi\ndanser\ndescendre\ndétendre\nentrer\nenviron\nmanquer\npantalon\nplan\nrencontrer\nrendre\nreprendre\ntendre\nventre\nviande"
+    },
+    {
+      "title": "Thème 6 - Semaine 3",
+      "text": "acrobate\nadulte\nboîte\ncompote\nconte\ndate\ndroite\njuste\nminute\npépite\npirate\nsorte\nsuite\ntriste\nvisite"
     }
-
     /*      
     
     
